72c2b1313c1aeaf7ab6463618f32dfaa65f3e5ab
Frederik De Roover <frederik@fraai.agency>
2025-06-15 20:25:35 +0200

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://nexxura.be",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    i18n: {
+        defaultLocale: "nl",
+        locales: ["nl"],
+        routing: {
+            prefixDefaultLocale: true
+        }
+    }
+});
