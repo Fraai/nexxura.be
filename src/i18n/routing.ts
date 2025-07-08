cc464679cc566eccdda87068bcce9a56a60457ec
Frederik De Roover <frederik@fraai.agency>
2025-07-08 20:52:02 +0200

    Validations

---
+++ src/i18n/routing.ts
@@ -0,0 +1,12 @@
+export const routes = {
+    nl: {
+        'services': 'diensten',
+        'contact': 'contact',
+        'about': 'over-ons',
+    },
+    en: {
+        'services': 'services',
+        'contact': 'contact',
+        'about': 'about-us',
+    }
+};
