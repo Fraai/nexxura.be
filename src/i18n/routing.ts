88d955ee4ef798db769916f39ae59ab82ebef295
Frederik De Roover <frederik@fraai.agency>
2025-07-09 20:24:00 +0200

    SEO tweaks

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,10 +1,12 @@
 export const routes = {
     nl: {
+        'home': 'Home',
         'services': 'diensten',
         'contact': 'contact',
         'about': 'over-ons',
     },
     en: {
+        'home': 'Home',
         'services': 'services',
         'contact': 'contact',
         'about': 'about-us',
